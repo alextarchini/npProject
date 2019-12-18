f88d5ce8f3d02d5b5292b862d2cde2638428cfc8
Aleksandra Tarchini <alex.tarchini@gmail.com>
2019-12-18 13:18:17 +0100

    online shop

--- app.js
+++ app.js
@@ -1,44 +1,112 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const graphqlHttp = require("express-graphql");
-const mongoose = require("mongoose");
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var expressHbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+var validator = require("express-validator");
+var MongoStore = require("connect-mongo")(session);
 
-const graphQlSchema = require("./graphql/schema/index");
-const graphQlResolvers = require("./graphql/resolvers/index");
-const isAuth = require("./middleware/is-auth");
+var app = express();
 
-const app = express();
+/* mongoose.connect("mongodb://localhost:27017/shopping"); */
+/* mongoose.connect(
+  "mongodb+srv://alex:<Jesus1986>@cluster0-avhlk.mongodb.net/shopping-cart?retryWrites=true&w=majority"
+); */
+/* mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-avhlk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+); */
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
-app.use(isAuth);
-
-app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: graphQlSchema,
-    rootValue: graphQlResolvers,
-    graphiql: true
-  })
-);
-
-mongoose
+/* mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-avhlk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8000);
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
+  }); */
+
+/* DB Connect */
+mongoose.connect(
+  "mongodb+srv://alex:123@cluster0-avhlk.mongodb.net/shopping?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+require("./config/passport");
+
+// view engine setup
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+  })
+);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.resolve("./public"))); //to show the images!!
+
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  next();
+});
+var routes = require("./routes/index");
+var userRoutes = require("./routes/user");
+
+app.use("/user", userRoutes);
+app.use("/", routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
   });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,22 +1,18 @@
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt-nodejs");
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdEvents: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Event"
-    }
-  ]
+var userSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
+
+userSchema.methods.encryptPassword = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
